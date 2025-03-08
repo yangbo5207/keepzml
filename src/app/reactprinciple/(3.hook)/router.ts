490d24cf7f3ae81332de5151fb80eb798eed44de
yangbo <1832064870@qq.com>
2025-03-08 21:09:27 +0800

    update

---
+++ src/app/reactprinciple/(3.hook)/router.ts
@@ -0,0 +1,22 @@
+export const r3 = [
+  {
+    type: 'tip',
+    name: 'Hook'
+  },
+  {
+    path: '0.hookbackground',
+    name: '0、hook 背景',
+  },
+  {
+    path: '1.hooktype',
+    name: '1、hook 分类',
+  },
+  {
+    path: '2.hookwithclosure',
+    name: '2、hook 与闭包',
+  },
+  {
+    path: '3.hookwithfiber',
+    name: '3、hook 与 fiber',
+  },
+]
--- src/app/reactprinciple/router.ts
+++ src/app/reactprinciple/router.ts
@@ -1,6 +1,7 @@
 import {r0} from './(0.eventloop)/router'
 import {r1} from './(1.concurrent)/router'
 import {r2} from './(2.fiber)/router'
+import {r3} from './(3.hook)/router'
 
 interface RouteItem {
   type?: string,
@@ -13,7 +14,8 @@ interface RouteItem {
 export const routers: RouteItem[] = [
   ...r0,
   ...r1,
-  ...r2
+  ...r2,
+  ...r3
 ].map(item => {
   if (item.path) {
     item.path = `/reactprinciple/${item.path}`
